19afa63c0f0f22d724557daff8597fe3c3dc45d1
stephen <stephen.a.atwood@gmail.com>
2016-03-20 00:54:50 -0500

    Set up basic app interface with navigation controller and image view.

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Stephen Atwood on 3/20/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    // MARK: properties
+    @IBOutlet var imageView: UIImageView!
+    
+}
