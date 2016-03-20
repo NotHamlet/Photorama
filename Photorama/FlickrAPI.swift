5b4a3d4cbbb90721835fa4874cd1b6a18006c7d0
stephen <stephen.a.atwood@gmail.com>
2016-03-20 01:38:56 -0500

    Added code to construct actual URL from given parameters in flickerURL() method

--- Photorama/FlickrAPI.swift
+++ Photorama/FlickrAPI.swift
@@ -15,13 +15,39 @@ enum Method: String {
 struct FlickerAPI {
     
     private static let baseURLString = "​h​t​t​p​s​:​/​/​a​p​i​.​f​l​i​c​k​r​.​c​o​m​/​s​e​r​v​i​c​e​s​/​r​e​s​t"
+    private static let APIKey = "a​6​d​8​1​9​4​9​9​1​3​1​0​7​1​f​1​5​8​f​d​7​4​0​8​6​0​a​5​a​8​8"
     
     static func recentPhotosURL() -> NSURL {
         return flickrURL(method: .RecentPhotos, parameters: ["extras": "url_h, date_taken"])
     }
     
     private static func flickrURL(method method: Method, parameters: [String:String]?) -> NSURL {
-        return NSURL()
+        let components = NSURLComponents(string: baseURLString)!
+        
+        var queryItems = [NSURLQueryItem]()
+        
+        let baseParams = [
+            "method": method.rawValue,
+            "format": "json",
+            "nojsoncallback": "1",
+            "api_key": APIKey
+        ]
+        
+        for (key, value) in baseParams {
+            let item = NSURLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        if let additionalParams = parameters {
+            for (key,value) in additionalParams {
+                let item = NSURLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        
+        components.queryItems = queryItems
+        
+        return components.URL!
     }
     
     
