8f1a24d08212c00a8720069ed180da5e36bbf073
stephen <stephen.a.atwood@gmail.com>
2016-03-22 13:37:51 -0500

    Updated View to use CollectionView and added some boilerplate to stock the view with unfinished items.

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Stephen Atwood on 3/22/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+}
